e4a85401921884afbd91df44f73a013fefebd850
Happydas <happy00701@gmail.com>
2018-04-21 14:01:54 +0200

    Changing

---
+++ venv/SPARQL/Person_Party_Before_Filtered.py
@@ -0,0 +1,17 @@
+newFile = open('../Data/Person_Party_Before_Filtered.txt', 'w', encoding="utf-8")
+
+Directions = False
+
+with open('../Data/Person_Party_Clean.txt', encoding="utf-8") as f:
+    for c in f.read():
+        if Directions is False and c == '(':
+            Directions = True
+        elif Directions is True and c == ')':
+            Directions = False
+            continue
+
+        if not Directions:
+            newFile.write(c)
+
+        if Directions:
+            pass
